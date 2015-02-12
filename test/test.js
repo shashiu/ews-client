5cde8fb71d208ba1c0626406ff6d82a1360573d8
Shashi Uli <shashi.uli@gmail.com>
2015-02-12 13:22:21 -0800

    Some refactoring to the APIs and tests

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 var ews = require('./../ews');
 
-// get web credetials from process environment
+// get web credentials from process environment
 var user = process.env.EWSUSER;
 var pass = process.env.EWSPASS;
 var domain = process.env.EWSDOMAIN;
@@ -12,49 +12,65 @@ if (user == null || pass == null || domain == null || mailbox == null) {
 
 // setup connection to Exchange
 var msExchange = new ews.MSExchange();
-var calendar = new ews.Calendar(msExchange);
-var contacts = new ews.Contacts(msExchange);
 
 // Autodiscover URL to get to exchange web service
 msExchange.setAuth(user, pass, domain);
 console.log("Autodiscover");
 msExchange.autoDiscover(mailbox)
-    .then(function () {
-        console.log("Autodiscover Success!");
-        // enumerate all room resources
-        return contacts.getRoomLists();
-    }).then(function (roomLists) {
+.then(function () {
+
+    console.log("Autodiscover Success!");
+    _testCalendarAPIs(msExchange);
+    _testContactAPIs(msExchange);
+
+}).catch(function (error) {
+    console.log("Error!", error);
+});
+
+_testCalendarAPIs = function (session) {
+    console.log('Getting calendar entries for next 24 hours for session calendar');
+    session.getFolder('calendar').then(function (folder) {
+        var calendar = new ews.Calendar(session, null, folder.FolderId, folder.ChangeKey);
+        calendar.getEntries().then(_displayCalendarEntries);
+        console.log('Getting calendar entries for next 24 hours for mailbox ' + mailbox);
+        var calendar2 = new ews.Calendar(session, mailbox);
+        calendar2.getEntries().then(_displayCalendarEntries);
+    });
+}
+
+_displayCalendarEntries = function (entries) {
+    console.log('----------------------------------------------------------------------------');
+    if (entries != null) {
+        for (var i = 0; i < entries.length; i++) {
+            // display the calendar entries for the room
+            console.log(entries[i].Subject);
+            console.log('    From: ' + entries[i].Start + ', To: ' + entries[i].End)
+            if (entries[i].MeetingUrl != null) {
+                // including meeting URL and audio information, if there is some
+                console.log('    Meeting URL:' + entries[i].MeetingUrl);
+                console.log('    AudioOptions:' + entries[i].GTMAudioOptions);
+                console.log('    Attendees:' + entries[i].Attendees);
+            }
+        }
+    }
+    console.log('----------------------------------------------------------------------------');
+}
+
+_testContactAPIs = function (session) {
+    var contacts = new ews.Contacts(session);
+    console.log("Getting room lists");
+    contacts.getRoomLists().then(function (roomLists) {
         if (roomLists == null) {
             console.log('    Room List is null (Exchange Admin hasn\'t configured them)');
         }
-        // enumerate calendar for a room
-        console.log('Getting calendar entries for next 24 hours for ' + mailbox);
-        console.log('----------------------------------------------------------------------------');
-        return calendar.getEntries(mailbox);
-    }).then(function (entries) {
-        if (entries != null) {
-            for (var i = 0; i < entries.length; i++) {
-                // display the calendar entries for the room
-                console.log(entries[i].Subject);
-                console.log('    From: ' + entries[i].Start + ', To: ' + entries[i].End)
-                if (entries[i].MeetingUrl != null) {
-                    // including meeting URL and audio information, if there is some
-                    console.log('    Meeting URL:' + entries[i].MeetingUrl);
-                    console.log('    AudioOptions:' + entries[i].GTMAudioOptions);
-                    console.log('    Attendees:' + entries[i].Attendees);
-                }
-            }
-        }
-        console.log('----------------------------------------------------------------------------');
+        console.log("Getting Contact details for " + mailbox);
         return contacts.getDetails(mailbox);
     }).then(function (contact) {
-        console.log("Contact info for: " + mailbox)
         console.log("    " + contact.DisplayName);
         console.log("    " + contact.Email);
         var i = 0;
         for (i = 0; i < contact.phoneNumbers.length; i++) {
             console.log("    " + contact.phoneNumbers[i]);
         }
-    }).catch(function (error) {
-        console.log("Error!", error);
     });
+}
